28950798f6c660c8e38f871646f20c187232fef8
Adam Liang <hello@adamliang.dev>
2025-01-29 17:52:55 -0500

    fixed ladle config

---
+++ .ladle/components.tsx
@@ -0,0 +1,2 @@
+import "../src/styles/_base.scss";
+import "../src/styles/story.scss";
--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('@ladle/react').UserConfig} */
 export default {
-  stories: "src/**/*.stories.{js,jsx,ts,tsx}",
-  port: 61000,
-  addons: {
-    a11y: {
-      enabled: true
-    },
-    action: {
-      enabled: true
-    },
-    control: {
-      enabled: true
-    },
-    theme: {
-      enabled: true
-    }
-  }
-};
+  stories: "src/components/**/*.stories.{js,jsx,ts,tsx}",
+  port: 61000};
